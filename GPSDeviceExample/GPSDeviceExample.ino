e07c2b04a1cf8612d723012d867e3830b1b966bc
Arka <arkaprava.123040@gmail.com>
2022-04-09 19:24:05 +0530

    Ublox Neo
    changes

--- GPSDeviceExample/GPSDeviceExample.ino
+++ GPSDeviceExample/GPSDeviceExample.ino
@@ -6,7 +6,7 @@
    It requires the use of SoftwareSerial, and assumes that you have a
    4800-baud serial GPS device hooked up on pins 4(rx) and 3(tx).
 */
-static const int RXPin = 17, TXPin = 16;
+static const int RXPin = 16, TXPin = 17;
 static const uint32_t GPSBaud = 9600;
 
 // The TinyGPSPlus object
